94ba7165e8be875801fefe60ddaf28e3439a000d
timolai-andrievich <timolai.andrievitch@ya.ru>
2022-07-26 17:27:25 +0300

    Added documentation to player.py

--- src/player.py
+++ src/player.py
@@ -1,3 +1,5 @@
+"""Contains classes encapsulating several game strategies.
+"""
 import random
 from typing import Dict
 
@@ -9,24 +11,55 @@ from model import Model
 from config import Config
 
 
-class Player:
+class Player:  # pylint: disable=too-few-public-methods
+    """Abstract class, represents a playing strategy.
+    """
 
     def get_action(self, game: Game) -> int:
-        pass
+        """Chooses action based on the class strategy.
+
+        Args:
+            game (Game)
+
+        Returns:
+            int: The ID of the chosen action.
+        """
 
 
-class RandomPlayer(Player):
+class RandomPlayer(Player):  # pylint: disable=too-few-public-methods
+    """Chooses a random legal action for every position.
+    """
 
     def get_action(self, game: Game) -> int:
+        """Chooses random legal action.
+
+        Args:
+            game (Game)
+
+        Returns:
+            int: The ID of the chosen action.
+        """
         return np.random.choice(game.get_actions())
 
 
 class MinMaxPlayer(Player):
+    """Chooses random action that will not lead to losing advantage based on minmax algorithm.
+    """
 
     def __init__(self):
         self.cache: Dict[str, int] = {}
 
     def evaluate(self, game: Game) -> int:
+        """Evaluates the current position in the game and returns 1 if X wins,
+        0 if the game will result in a draw, -1 if O wins. Evaluation is based on
+        assumption that both players will play optimal moves.
+
+        Args:
+            game (Game): Game to evaluate.
+
+        Returns:
+            int: Evaluation result.
+        """
         image = game.position.to_image()
         if image in self.cache:
             return self.cache[image]
@@ -44,6 +77,14 @@ class MinMaxPlayer(Player):
         return self.cache[image]
 
     def get_action(self, game: Game) -> int:
+        """Chooses random optial action.
+
+        Args:
+            game (Game)
+
+        Returns:
+            int: The ID of the chosen action.
+        """
         evaluation = self.evaluate(game)
         moves = []
         for action in game.get_actions():
@@ -54,26 +95,59 @@ class MinMaxPlayer(Player):
         return random.choice(moves)
 
 
-class MctsPlayer(Player):
+class MctsPlayer(Player):  # pylint: disable=too-few-public-methods
+    """Chooses action based on Monte-Carlo Tree Search algorithm.
+    Uses neural network as a policy function.
+    """
 
-    def __init__(self, nn: Model, config: Config):
-        self.nn = nn
+    def __init__(self, model: Model, config: Config):
+        """Chooses action based on Monte-Carlo Tree Search algorithm.
+        Uses neural network as a policy function.
+
+        Args:
+            model (Model): Neural network to be used as a policy function.
+            config (Config): Parameters for model and MCTS.
+        """
+        self.model = model
         self.config = config
 
     def get_action(self, game: Game) -> int:
+        """Chooses legal action using NN and MCTS.
+
+        Args:
+            game (Game)
+
+        Returns:
+            int: The ID of the chosen action.
+        """
         tree = MCTS(self.config)
-        actions, results = tree.run(game.copy(), self.nn.policy_function)
+        actions, _ = tree.run(game.copy(), self.model.policy_function)
         action = np.argmax(actions)
         return int(action)
 
 
-class ModelPlayer(Player):
+class ModelPlayer(Player):  # pylint: disable=too-few-public-methods
+    """Chooses action based on pure neural network evaluation, without the MCTS algorithm.
+    """
 
-    def __init__(self, nn: Model):
-        self.nn = nn
+    def __init__(self, model: Model):
+        """Chooses action based on pure neural network evaluation, without the MCTS algorithm.
+
+        Args:
+            model (Model): Neural network to be used as a policy function.
+        """
+        self.model = model
 
     def get_action(self, game: Game) -> int:
-        actions, results = self.nn.policy_function(game.position)
+        """Chooses legal action using NN.
+
+        Args:
+            game (Game)
+
+        Returns:
+            int: The ID of the chosen action.
+        """
+        actions, _ = self.model.policy_function(game.position)
         legal_actions = game.get_actions()
         action = max(legal_actions, key=lambda x: actions[x])
         return action
